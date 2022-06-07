2752bf15eb16b40f56af8823a2568e01a8c3bae8
Nirwash <nirwash@bk.ru>
2022-06-07 21:17:55 +0300

    Initial commit

---
+++ app/src/main/java/com/nirwashh/android/teleglam/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.nirwashh.android.teleglam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import com.nirwashh.android.teleglam.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var b: ActivityMainBinding
+    private lateinit var drawer: Drawer
+    private lateinit var header: AccountHeader
+    private lateinit var toolbar: Toolbar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunc()
+    }
+
+    private fun initFunc() {
+        setSupportActionBar(toolbar)
+        createHeader()
+        createDrawer()
+    }
+
+    private fun createDrawer() {
+        drawer = DrawerBuilder()
+            .withActivity(this)
+            .withToolbar(toolbar)
+            .withActionBarDrawerToggle(true)
+            .withSelectedItem(-1)
+            .withAccountHeader(header)
+            .addDrawerItems(
+                PrimaryDrawerItem()
+                    .withIdentifier(100)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать группу")
+                    .withSelectable(false)
+
+            )
+            .build()
+    }
+
+    private fun createHeader() {
+        header = AccountHeaderBuilder()
+            .withActivity(this)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                ProfileDrawerItem().withName("Mike Taison")
+                    .withEmail("+79891111111")
+            )
+            .build()
+    }
+
+    private fun initFields() {
+        toolbar = b.mainToolbar
+
+    }
+}
