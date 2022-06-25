05ad1e6407af4782ed013f2efefbf60d8e1fa9fc
Nirwash <nirwash@bk.ru>
2022-06-25 20:37:11 +0300

    добавление меню в SettingsFragment.kt

--- app/src/main/java/com/nirwashh/android/teleglam/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/nirwashh/android/teleglam/ui/fragments/SettingsFragment.kt
@@ -1,7 +1,17 @@
 package com.nirwashh.android.teleglam.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.nirwashh.android.teleglam.R
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
+    override fun onResume() {
+        super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
+    }
 
 }
